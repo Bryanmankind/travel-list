17c400df52743d24b59906dc1fe238cdcb3686b9
Bryanmankind <shegeemankind@gmail.com>
2024-03-29 11:40:00 +0100

    react-travel-list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,11 +1,34 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
 function Logo() {
   return <h1>✈ Far Awway 🌍</h1>;
 }
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip 👜</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">List</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you alredy packed X</em>
+    </footer>
+  );
+}
