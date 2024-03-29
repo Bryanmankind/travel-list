b695da5468531c436954e984c6d4c7c455b80676
Bryanmankind <shegeemankind@gmail.com>
2024-03-29 12:04:58 +0100

    react-travel-list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,7 +27,24 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((map) => (
+        <List items={map.id} />
+      ))}
+    </ul>
+  );
+}
+
+function List({ items }) {
+  return (
+    <li>
+      {items.id}
+      {items.description}
+      {items.quantity}
+      {items.packed}
+    </li>
+  );
 }
 
 function Stats() {
